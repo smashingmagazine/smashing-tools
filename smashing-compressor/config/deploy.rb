cb90fad941e1b7baba1516d12140ae0b7ce55129
Frédéric Maquin <frederic.maquin@smashingmagazine.com>
2013-07-23 17:07:58 +0200

    Basic capistrano install.

---
+++ smashing-compressor/config/deploy.rb
@@ -0,0 +1,44 @@
+set :deploy_to, "/var/www/playground.smashingmagazine.com/smashing-compressor"
+set :domain, 'playground.smashingmagazine.com'
+role :web, 'playground.smashingmagazine.com'
+
+set :application, "wallpaper-form-generator"
+set :deploy_via, :copy
+#set :copy_cache, true
+set :copy_exclude, [".git/*", "**/._*", "**/.DS_Store"]
+
+set :repository, "build"
+set :scm, :none
+set :copy_compression, :gzip
+set :use_sudo, false
+set :user, 'deploy'
+set :port, 122
+
+default_run_options[:pty] = true
+
+before 'deploy:update_code', "deploy:build_assets"
+
+namespace :install
+  task :debian
+    
+  if (run 'gem list image_optim -i')
+    sudo 'gem install image_optim'
+  end
+    
+    
+    sudo 'apt-get install '
+  end
+end
+
+namespace :deploy do
+  task :build_assets do
+    run_locally 'rm -rf build/ && mkdir build'
+    run_locally 'cp -r source/images source/includes source/index.php build'
+    run_locally 'mkdir build/stylesheets build/javascripts'
+    
+    run_locally 'imageoptim -a -q -d source/images'
+    
+    run_locally 'closure-compiler --compilation_level WHITESPACE_ONLY --js source/javascripts/jquery-1.10.1.min.js --js source/javascripts/parsley.min.js --js source/javascripts/main.js --js_output_file build/javascripts/app.js'
+    run_locally 'yuicompressor source/stylesheets/main.css -o build/stylesheets/app.css'
+  end
+end
